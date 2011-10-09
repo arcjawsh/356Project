7c4a9eff7306419478f25c484af6d711b908ba55
arcjawsh <woodjosh@bigpond.net.au>
2011-10-09 18:24:16 +1100

    Added Working Heightmap - Josh

--- GameProject/GameProject/Game1.cs
+++ GameProject/GameProject/Game1.cs
@@ -19,73 +19,99 @@ namespace GameProject
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        Matrix projectionMatrix;
+        Matrix viewMatrix;
+
+        Model terrain;
+        HeightMapInfo heightMapInfo;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+       
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(
+               MathHelper.ToRadians(45.0f), GraphicsDevice.Viewport.AspectRatio, 1f, 10000);
+            viewMatrix = Matrix.CreateLookAt(new Vector3(0, 0, 2),
+            new Vector3(0, 0, 0), Vector3.Up);
 
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+       
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
             // TODO: use this.Content to load your game content here
+
+            terrain = Content.Load<Model>("terrain");
+            // The terrain processor attached a HeightMapInfo to the terrain model's
+           
+            heightMapInfo = terrain.Tag as HeightMapInfo;
+            if (heightMapInfo == null)
+            {
+                string message = "The terrain model did not have a HeightMapInfo " +
+                    "object attached. Are you sure you are using the " +
+                    "TerrainProcessor?";
+                throw new InvalidOperationException(message);
+            }
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
+       
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+       
         protected override void Update(GameTime gameTime)
         {
-            // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                this.Exit();
-
-            // TODO: Add your update logic here
+            //Handle Input
+            //Update Camera
 
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+       
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Add your drawing code here
-
+            DrawModel(terrain);
             base.Draw(gameTime);
+        }
+
+        void DrawModel(Model model)
+        { //common function for drawing a model
+            Matrix[] boneTransforms = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(boneTransforms);
+
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = boneTransforms[mesh.ParentBone.Index];
+                    effect.View = viewMatrix;
+                    effect.Projection = projectionMatrix;
+
+                    effect.EnableDefaultLighting();
+                    effect.PreferPerPixelLighting = true;
+
+                    // Set the fog to match the black background color
+                    effect.FogEnabled = true;
+                    effect.FogColor = Vector3.Zero;
+                    effect.FogStart = 1000;
+                    effect.FogEnd = 3200;
+                }
+
+                mesh.Draw();
+            }
         }
     }
 }
